59f8b4e3a4f97cb2f366401205718a078c31b800
Guillermo Rauch <rauchg@gmail.com>
2017-11-04 19:53:11 -0700

    fix linting

--- test.js
+++ test.js
@@ -1,25 +1,24 @@
 /* eslint-env jest*/
-const {createServer} =  require('http');
-const cachedDNSFetch = require('./index')();
+const { createServer } = require('http')
+const cachedDNSFetch = require('./index')()
 
 test('works with localhost', async () => {
-  let i = 0
   const server = createServer((req, res) => {
-    res.end('ha');
-  });
+    res.end('ha')
+  })
 
   return new Promise((resolve, reject) => {
     server.listen(async () => {
-      const {port} = server.address();
+      const { port } = server.address()
       try {
-        const res = await cachedDNSFetch(`http://127.0.0.1:${port}`);
-        expect(await res.text()).toBe('ha');
-        server.close();
-        resolve();
+        const res = await cachedDNSFetch(`http://127.0.0.1:${port}`)
+        expect(await res.text()).toBe('ha')
+        server.close()
+        resolve()
       } catch (err) {
-        reject(err);
+        reject(err)
       }
-    });
-    server.on('error', reject);
-  });
-});
+    })
+    server.on('error', reject)
+  })
+})
