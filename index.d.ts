6e13a3c0f68722a6b72ab9578dbe28583539a3dc
Dylan Frankland <dfrankland@users.noreply.github.com>
2022-01-05 08:37:49 -0800

    Update index.d.ts
    Adds the proper typing to the `createFetch` function based on https://github.com/vercel/fetch/blob/d80d8aef54efc9e090cb851bd0f943a7e5d1b090/index.d.ts#L1

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,22 @@
-import { Request, RequestInit, Response } from 'node-fetch';
+import * as http from 'http';
+import * as https from 'http';
+import { Headers, Request, RequestInit, Response } from 'node-fetch';
 
-export default function createFetch(): (
+export type FetchOptions = RequestInit & {
+	agent?: https.Agent | http.Agent;
+};
+
+export type Fetch = (
+	url: string | Request,
+	options?: FetchOptions
+) => Promise<Response>;
+
+export type FetchModule = {
+	default: Fetch;
+	Headers: typeof Headers;
+};
+
+export default function createFetch(fetchModule?: FetchModule): (
     url: string | Request,
     init?: RequestInit
 ) => Promise<Response>;
